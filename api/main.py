b965b0175755e7c30bfd95883bd611fff28783cf
alf4ridzi <aalalfaridzi9@gmail.com>
2024-05-18 21:29:51 +0700

    Change port default server

--- api/main.py
+++ api/main.py
@@ -433,4 +433,4 @@ def home():
     return "API for SimpleCoin Project"
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True, port='5000')
+    app.run(host='0.0.0.0', debug=True, port='5000')
